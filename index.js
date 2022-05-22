4fa09a47149b4e790eaac7912fa6bcd021c20408
AltF4irl <73783095+AltF4irl@users.noreply.github.com>
2022-05-22 22:05:46 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const n = document.querySelector(".name");
+const s = document.querySelector(".surname");
+const e = document.querySelector(".email");
+const sub = document.querySelector(".btn");
+
+const api_key ="UxVG1oTRq4R6CVcrwkuG9PjV72aV7u6o";
+
+
+sub.addEventListener('click', function() {
+    console.log("hehe");
+
+    var myHeaders = new Headers();
+    myHeaders.append("apikey", "UxVG1oTRq4R6CVcrwkuG9PjV72aV7u6o");
+
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+    };
+    if(e.value !== "") {
+        fetch("https://api.apilayer.com/email_verification/check?email="+e.value, requestOptions)
+    .then(response => response.text())
+    .then(result => params = [JSON.parse(result).format_valid, JSON.parse(result).mx_found, JSON.parse(result).smtp_check])
+    // .then(result => console.log(result.format_valid, result.mx_found))
+    // .then(result => console.log(JSON.parse(result)))
+    .then(() => {
+        if(params[0] == true && params[1] == true) {
+            alert("Email Valid");
+        } else {
+            alert("Email Fake")
+        }
+    })
+    .catch(error => console.log('error', error));
+    }else {
+        alert("please enter an email");
+    }
+    
+})
+
+
+
+
